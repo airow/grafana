7c320eb19f034792a787a020ed6a14ce8d913b6d
zhangchen <zhangchen2@teld.cn>
2017-05-06 13:49:18 +0800

    自定义数据源及iframe面板。 控制器中templateUrl可以使用相对目录，不已public开始的都按相对路径加载 指令中的templateUrl需要使用绝对路径，以public开头

---
+++ public/app/plugins/panel/teld-iframe-panel/module.ts
@@ -0,0 +1,87 @@
+///<reference path="../../../headers/common.d.ts" />
+
+import _ from 'lodash';
+import angular from 'angular';
+import {PanelCtrl} from 'app/plugins/sdk';
+
+export class TeldIframePanelCtrl extends PanelCtrl {
+  static templateUrl = `partials/module.html`;
+
+  remarkable: any;
+  content: string;
+  // Set and populate defaults
+  panelDefaults = {
+    mode    : "markdown", // 'html', 'markdown', 'text'
+    content : "# title",
+  };
+
+  /** @ngInject **/
+  constructor($scope, $injector, private templateSrv, private $sce) {
+    super($scope, $injector);
+
+    _.defaults(this.panel, this.panelDefaults);
+
+    this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+    this.events.on('refresh', this.onRefresh.bind(this));
+    this.events.on('render', this.onRender.bind(this));
+  }
+
+  sendMessage(){
+    this.$scope.$emit('$messageOutgoing', angular.toJson({"response" : "I'm grafana."}));
+  };
+
+  onInitEditMode() {
+    this.addEditorTab('Options', 'partials/editor.html');
+    this.editorTabIndex = 1;
+
+    if (this.panel.mode === 'text') {
+      this.panel.mode = 'markdown';
+    }
+  }
+
+  onRefresh() {
+    this.render();
+  }
+
+  onRender() {
+    if (this.panel.mode === 'markdown') {
+      this.renderMarkdown(this.panel.content);
+    } else if (this.panel.mode === 'html') {
+      this.updateContent(this.panel.content);
+    }
+    this.renderingCompleted();
+  }
+
+  renderText(content) {
+    content = content
+    .replace(/&/g, '&amp;')
+    .replace(/>/g, '&gt;')
+    .replace(/</g, '&lt;')
+    .replace(/\n/g, '<br/>');
+    this.updateContent(content);
+  }
+
+  renderMarkdown(content) {
+    if (!this.remarkable) {
+      return System.import('remarkable').then(Remarkable => {
+        this.remarkable = new Remarkable();
+        this.$scope.$apply(() => {
+          this.updateContent(this.remarkable.render(content));
+        });
+      });
+    }
+
+    this.updateContent(this.remarkable.render(content));
+  }
+
+  updateContent(html) {
+    try {
+      this.content = this.$sce.trustAsHtml(this.templateSrv.replace(html, this.panel.scopedVars));
+    } catch (e) {
+      console.log('Text panel error: ', e);
+      this.content = this.$sce.trustAsHtml(html);
+    }
+  }
+}
+
+export {TeldIframePanelCtrl as PanelCtrl}
