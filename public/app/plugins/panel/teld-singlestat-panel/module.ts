620b4fbdec63952f880c5e09d5bb308e6acf3fd7
zhangchen <zhangchen2@teld.cn>
2018-06-08 21:44:54 +0800

    切屏按钮字体大小

--- public/app/plugins/panel/teld-singlestat-panel/module.ts
+++ public/app/plugins/panel/teld-singlestat-panel/module.ts
@@ -15,8 +15,10 @@ import { finglestatEchartsEventEditorComponent } from '../teld-eventhandler-edit
 import { submenuDirective } from '../../../features/dashboard/submenu/submenu';
 
 loadPluginCssPath({
-  dark: '/public/app/plugins/panel/teld-iframe-panel/css/teld-iframe-panel.built-in.css',
-  light: '/public/app/plugins/panel/teld-iframe-panel/css/teld-iframe-panel.built-in.css',
+  //cssPath: '/public/app/plugins/panel/teld-singlestat-panel/css/singlestat.css',
+  dark: '/public/app/plugins/panel/teld-singlestat-panel/css/singlestat.built-in.css',
+  light: '/public/app/plugins/panel/teld-singlestat-panel/css/singlestat.built-in.css',
+  //light: '/public/app/plugins/panel/teld-singlestat-panel/css/singlestat.built-in.css'
 });
 
 class SingleStatCtrl extends MetricsPanelCtrl {
