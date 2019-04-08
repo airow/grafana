32ee3004624893fcf2af6f47b181df0cedbb5a44
zhangchen <zhangchen2@teld.cn>
2019-04-08 09:57:00 +0800

    interpolateTemplateVars 过滤器支持表达式变量解析

--- public/app/core/filters/filters.ts
+++ public/app/core/filters/filters.ts
@@ -5,22 +5,22 @@ import angular from 'angular';
 import moment from 'moment';
 import coreModule from '../core_module';
 
-coreModule.filter('stringSort', function() {
-  return function(input) {
+coreModule.filter('stringSort', function () {
+  return function (input) {
     return input.sort();
   };
 });
 
-coreModule.filter('slice', function() {
-  return function(arr, start, end) {
+coreModule.filter('slice', function () {
+  return function (arr, start, end) {
     if (!_.isUndefined(arr)) {
       return arr.slice(start, end);
     }
   };
 });
 
-coreModule.filter('stringify', function() {
-  return function(arr) {
+coreModule.filter('stringify', function () {
+  return function (arr) {
     if (_.isObject(arr) && !_.isArray(arr)) {
       return angular.toJson(arr);
     } else {
@@ -29,8 +29,8 @@ coreModule.filter('stringify', function() {
   };
 });
 
-coreModule.filter('moment', function() {
-  return function(date, mode) {
+coreModule.filter('moment', function () {
+  return function (date, mode) {
     switch (mode) {
       case 'ago':
         return moment(date).fromNow();
@@ -39,18 +39,18 @@ coreModule.filter('moment', function() {
   };
 });
 
-coreModule.filter('noXml', function() {
-  var noXml = function(text) {
-  return _.isString(text)
-    ? text
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/'/g, '&#39;')
-    .replace(/"/g, '&quot;')
-    : text;
+coreModule.filter('noXml', function () {
+  var noXml = function (text) {
+    return _.isString(text)
+      ? text
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/'/g, '&#39;')
+        .replace(/"/g, '&quot;')
+      : text;
   };
-  return function(text) {
+  return function (text) {
     return _.isArray(text)
       ? _.map(text, noXml)
       : noXml(text);
@@ -58,7 +58,7 @@ coreModule.filter('noXml', function() {
 });
 
 coreModule.filter('interpolateTemplateVars', function (templateSrv) {
-  var filterFunc: any = function(text, scope) {
+  var filterFunc: any = function (text, scope) {
     var scopedVars;
     if (scope.ctrl) {
       scopedVars = (scope.ctrl.panel || scope.ctrl.row).scopedVars;
@@ -66,6 +66,11 @@ coreModule.filter('interpolateTemplateVars', function (templateSrv) {
       scopedVars = scope.row.scopedVars;
     }
 
+    var filterFun = function (item) {
+      return item.type === 'teldExpression' && "es" === (item.filter || "es");
+    };
+    var scopedExpressionVars = templateSrv.teldExpression2ScopedVarsFormCache('TSG', {}, 'lucene', filterFun);
+    text = templateSrv.replaceWithText(text, scopedExpressionVars);
     return templateSrv.replaceWithText(text, scopedVars);
   };
 
