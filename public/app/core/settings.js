1afffd9c118e2a603346dc1d934f02712ae0db61
zhangchen <zhangchen2@teld.cn>
2019-11-24 10:48:05 +0800

    修改GrafanaTitle为大数据可视化平台

--- public/app/core/settings.js
+++ public/app/core/settings.js
@@ -7,7 +7,7 @@ function (_) {
   return function Settings (options) {
     var defaults = {
       datasources                   : {},
-      window_title_prefix           : 'Grafana - ',
+      window_title_prefix           : '大数据可视化平台 - ',
       panels                        : {},
       new_panel_title: 'Panel Title',
       playlist_timespan: "1m",
