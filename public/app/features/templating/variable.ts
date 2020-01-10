e0fad343ca9d65676aaf703a693e45e21c067dfa
zhangchen <zhangchen2@teld.cn>
2020-01-10 18:33:01 +0800

    postgre-datasource merge

--- public/app/features/templating/variable.ts
+++ public/app/features/templating/variable.ts
@@ -18,6 +18,34 @@ export {
   assignModelProperties
 };
 
+export const SEARCH_FILTER_VARIABLE = '__searchFilter';
+
+export const containsSearchFilter = (query: string): boolean =>
+  query && typeof query === 'string' ? query.indexOf(SEARCH_FILTER_VARIABLE) !== -1 : false;
+
+export const getSearchFilterScopedVar = (args: {
+  query: string;
+  wildcardChar: string;
+  options: { searchFilter?: string };
+}) => {
+  const { query, wildcardChar } = args;
+  if (!containsSearchFilter(query)) {
+    return {};
+  }
+
+  let { options } = args;
+
+  options = options || { searchFilter: '' };
+  const value = options.searchFilter ? `${options.searchFilter}${wildcardChar}` : `${wildcardChar}`;
+
+  return {
+    __searchFilter: {
+      value,
+      text: '',
+    },
+  };
+};
+
 export function containsVariable(...args: any[]) {
   var variableName = args[args.length-1];
   var str = args[0] || '';
