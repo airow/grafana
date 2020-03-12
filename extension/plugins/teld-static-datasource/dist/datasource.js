87b95de93ab65fe47b794c6f13248f9289742a1e
zhangchen <zhangchen2@teld.cn>
2020-03-12 22:06:21 +0800

    静态数据源dist目录

--- extension/plugins/teld-static-datasource/dist/datasource.js
+++ extension/plugins/teld-static-datasource/dist/datasource.js
@@ -64,7 +64,7 @@ System.register(["lodash", "moment", "./libs/node-where-filter"], function (_exp
                 return item.type === 'teldExpression' && "es" === (item.filter || "es");
               };
 
-              var scopedExpressionVars = this.templateSrv.teldExpression2ScopedVarsFormCache('Tstatic', options.scopedVars, 'lucene', filterFun);
+              var scopedExpressionVars = this.templateSrv.teldExpressionInDataSource2ScopedVarsFormCache(options, 'Tstatic', options.scopedVars, 'lucene', filterFun);
               where = this.templateSrv.replaceScopedVars(where, Object.assign({}, options.scopedVars, scopedExpressionVars));
               where = this.templateSrv.replace(where, Object.assign({}, options.scopedVars, scopedExpressionVars));
               var exprTree = wherefilter.makeTree(where);
